de0a57b34e7ca9dcbf8afeefe8d45f9e1d62c945
Harrison <imthatguyh9@gmail.com>
2023-10-07 15:48:56 +1300

    more details button + package.json update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,8 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.article.create({
-    data: {
+  const articles = [
+    {
       title: "Sample Article Title",
       author: "John Doe",
       date: new Date(),
@@ -15,11 +15,9 @@ async function main() {
       type_of_research: "Sample Type of Research",
       type_of_participant: "Sample Type of Participant",
       approved: true,
-      checked: true
+      checked: true,
     },
-  });
-  await prisma.article.create({
-    data: {
+    {
       title: "Sample Article Title 2",
       author: "John Doe",
       date: new Date(),
@@ -30,9 +28,67 @@ async function main() {
       type_of_research: "Sample Type of Research",
       type_of_participant: "Sample Type of Participant",
       approved: false,
-      checked: false
+      checked: false,
     },
-  });
+    {
+      title: "Effective SE Practices in Modern Web Development",
+      author: "John Doe",
+      date: new Date("2021-01-01"),
+      journal_name: "Journal of Software Engineering",
+      se_practice: "Continuous Integration",
+      claim: "Improves Deployment Frequency",
+      result_of_evidence: "Strongly Supportive",
+      type_of_research: "Empirical Study",
+      type_of_participant: "Professional Developers",
+      approved: true,
+      checked: true,
+    },
+    {
+      title: "Agile vs Waterfall: A Comparative Analysis",
+      author: "Alice Waters",
+      date: new Date("2019-05-15"),
+      journal_name: "Software Design Monthly",
+      se_practice: "Agile Development",
+      claim: "Enhances Collaboration",
+      result_of_evidence: "Moderately Supportive",
+      type_of_research: "Case Study",
+      type_of_participant: "Software Engineers",
+      approved: false,
+      checked: false,
+    },
+    {
+      title: "The Impact of Pair Programming on Software Quality",
+      author: "Bob Ross",
+      date: new Date("2020-10-01"),
+      journal_name: "Journal of Software Engineering",
+      se_practice: "Pair Programming",
+      claim: "Improves Code Quality",
+      result_of_evidence: "Strongly Supportive",
+      type_of_research: "Empirical Study",
+      type_of_participant: "Professional Developers",
+      approved: true,
+      checked: true,
+    },
+    {
+      title: "Impact of TDD on Software Defect Rates",
+      author: "Robert Smith",
+      date: new Date("2020-12-10"),
+      journal_name: "Testing Times Journal",
+      se_practice: "Test-Driven Development",
+      claim: "Reduces Bugs and Defects",
+      result_of_evidence: "Strongly Supportive",
+      type_of_research: "Empirical Study",
+      type_of_participant: "QA Engineers",
+      approved: true,
+      checked: false,
+    },
+  ];
+
+  for (const article of articles) {
+    await prisma.article.create({
+      data: article,
+    });
+  }
 }
 
 main()
