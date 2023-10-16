5688e0349c2d81f6057c01c1b99e8daeb7991233
BlakeFernandes <blakef148@gmail.com>
2023-10-17 11:56:03 +1300

    new index fiels!

---
+++ src/pages/administrator/old.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "~/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "~/components/ui/card"
+import type { Article } from "~/types";
+import { api } from "~/utils/api";
+
+interface ArticleListProps {
+    article: Article;
+}
+
+const AdminArticleView: React.FC<ArticleListProps> = ({ article }) => {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    <div className="flex justify-between w-full">
+                        {article.title}
+                        <Link href={`administrator/edit/${article.id}`}><Button>Edit</Button></Link>
+                    </div>
+                </CardTitle>
+                <CardDescription>{article.author} • {article.date.toDateString()}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>Journal Name: {article.journal_name}</p>
+                <p>SE Practice: {article.se_practice}</p>
+                <p>Claim: {article.claim}</p>
+                <p>Result of Evidence: {article.result_of_evidence}</p>
+                <p>Type of Research: {article.type_of_research}</p>
+                <p>Type of Participant: {article.type_of_participant}</p>
+                <p>Approved: {article.approved ? "Approved" : "Unapproved"}</p>
+            </CardContent>
+            <CardFooter>
+                <p>Created: {article.createdAt?.toDateString()} Last Updated: {article.updatedAt?.toDateString()}</p>
+            </CardFooter>
+        </Card>
+    );
+}
+
+const AdminArticleList = () => {
+    const articlesQuery = api.articles.getAll.useQuery();
+
+    return (
+        <div className="space-y-2">
+            {articlesQuery?.data?.map((article) => (
+                <AdminArticleView key={article.id} article={article} />
+            ))}
+        </div>
+    )
+};
+
+export default AdminArticleList;
--- src/pages/moderator/index.tsx
+++ src/pages/moderator/index.tsx
@@ -1,67 +1,149 @@
-import React, { useState, useEffect } from "react";
-import ArticleList, { Article } from "./ArticleList";
+import React, { useState, useEffect, useMemo } from "react";
+import Search from "~/components/Search";
+import ArticleDetail from "~/components/ArticleDetail";
+import styles from "~/styles/User.module.css";
 import { api } from "~/utils/api";
+import { Article } from "~/types";
+import { Button } from "~/components/ui/button";
+import { NavigationMenuDemo } from "~/components/Navigation";
 
-const ModeratorPage: React.FC = () => {
-  const articlesQuery = api.articles.getAll.useQuery();
-  const articleMutation = api.articles.update.useMutation({ onSuccess: () => articlesQuery.refetch() });
-  const createArticleMutation = api.articles.create.useMutation({ onSuccess: () => articlesQuery.refetch() });
-  
+
+const User: React.FC = () => {
   const [articles, setArticles] = useState<Article[]>([]);
+  const [allArticles, setAllArticles] = useState<Article[]>([]);
+  const [query, setQuery] = useState("");
+  const [selectedYear, setSelectedYear] = useState<string | null>(null);
+  const [sortField, setSortField] = useState<keyof Article | null>(null);
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+  const articlesQuery = api.articles.getAll.useQuery();
+  const [openDetails, setOpenDetails] = useState<string[]>([]);
+
+  const toggleDetails = (articleId: string) => {
+    setOpenDetails((prevState) => {
+      if (prevState.includes(articleId)) {
+        return prevState.filter((id) => id !== articleId);
+      }
+      return [...prevState, articleId];
+    });
+  };
 
   useEffect(() => {
     if (articlesQuery.data) {
-      setArticles(articlesQuery.data);
+      setAllArticles(articlesQuery?.data);
     }
-  }, [articlesQuery.data])
+  }, [articlesQuery.data]);
 
-  const handleApprove = (articleId: string) => {
-    articleMutation.mutate({ id: articleId, approved: true, checked: true });
+  const allAvailableYears = useMemo(() => {
+    return Array.from(
+      new Set(allArticles.map((a) => new Date(a.date).getFullYear())),
+    ).sort((a, b) => b - a);
+  }, [allArticles]);
+
+  const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedYear(event.target.value || null);
   };
 
-  const handleDeny = (articleId: string) => {
-    articleMutation.mutate({ id: articleId, approved: false, checked: true });
-  };
+  useEffect(() => {
+    const lowercasedQuery = query.toLowerCase();
+    let filteredData = allArticles.filter((article) => {
+      return article.se_practice.toLowerCase().includes(lowercasedQuery);
+    });
+    if (selectedYear) {
+      filteredData = filteredData.filter((article) => {
+        return new Date(article.date).getFullYear() === parseInt(selectedYear);
+      });
+    }
+    setArticles(filteredData);
+  }, [query, selectedYear, allArticles]);
 
-  const addNewArticle = () => {
-    const newArticle = {
-      title: "Sample Article Title",
-      author: "John Doe",
-      date: new Date(),
-      journal_name: "Journal of Sample Articles",
-      se_practice: "TDD",
-      claim: "Sample Claim",
-      result_of_evidence: "Sample Result of Evidence",
-      type_of_research: "Sample Type of Research",
-      type_of_participant: "Sample Type of Participant",
-      approved: false,
-      checked: false
-    };
+  const sortedArticles = useMemo(() => {
+    const sorted = [...articles];
+    if (sortField) {
+      sorted.sort((a, b) => {
+        if (a[sortField] && b[sortField]) {
+          if (a[sortField]! < b[sortField]!)
+            return sortDirection === "asc" ? -1 : 1;
+          if (a[sortField]! > b[sortField]!)
+            return sortDirection === "asc" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sorted;
+  }, [articles, sortField, sortDirection]);
 
-    createArticleMutation.mutate(newArticle);
-  };
-
-  const addButtonStyle = {
-    backgroundColor: '#008CBA',
-    color: 'white',
-    padding: '10px 20px',
-    margin: '10px 0',
-    cursor: 'pointer',
-    border: 'none',
-    borderRadius: '4px',
+  const handleSort = (field: keyof Article) => {
+    if (sortField === field) {
+      setSortDirection((prev) => (prev === "asc" ? "desc" : "asc"));
+    } else {
+      setSortField(field);
+      setSortDirection("asc");
+    }
   };
 
   return (
-    <div>
-      <h1>Modertor Dashboard</h1>
-      <ArticleList
-        articles={articles}
-        onApprove={handleApprove}
-        onDeny={handleDeny}
-      />
-      <button onClick={addNewArticle} style={addButtonStyle}>Add New Article</button>
-    </div>
+    <>
+      <NavigationMenuDemo />
+      
+      <div className={styles.container}>
+        <div className={styles.searchWrapper}>
+          <Search onUpdate={setQuery} />
+          {/* <select
+          className={styles.inputElement}
+          onChange={handleYearChange}
+          value={selectedYear ?? ""}
+        >
+          <option value="">All Years</option>
+          {allAvailableYears.map((year) => (
+            <option key={year} value={year.toString()}>
+              {year}
+            </option>
+          ))}
+        </select> */}
+          {/* Tony didnt like the filter by year button :( */}
+        </div>
+        <table className={styles.articlesTable}>
+          <thead>
+            <tr>
+              <th onClick={() => handleSort("title")}>Title</th>
+              <th onClick={() => handleSort("author")}>Author</th>
+              <th onClick={() => handleSort("date")}>Date</th>
+              <th onClick={() => handleSort("journal_name")}>Journal Name</th>
+              <th onClick={() => handleSort("se_practice")}>SE Practice</th>
+              <th className={styles.detailsColumn}>More Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedArticles.map((article) => (
+              <React.Fragment key={article.id}>
+                <tr>
+                  <td>{article.title}</td> <td>{article.author}</td>
+                  <td>{new Date(article.date).toLocaleDateString()}</td>
+                  <td>{article.journal_name}</td> <td>{article.se_practice}</td>
+                  <td className={`flex justify-center ${styles.detailsColumn}`}>
+                    <Button
+                      variant={openDetails.includes(article.id!) ? "destructive" : "default"}
+                      className={styles.buttonFullWidth}
+                      onClick={() => toggleDetails(article.id!)}
+                    >
+                      {openDetails.includes(article.id!) ? "Close" : "More"}
+                    </Button>
+                  </td>
+                </tr>
+                {openDetails.includes(article.id!) && (
+                  <tr>
+                    <td colSpan={6} className={styles.articleDetails}>
+                      <ArticleDetail article={article} />
+                    </td>
+                  </tr>
+                )}
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
-export default ModeratorPage;
+export default User;
